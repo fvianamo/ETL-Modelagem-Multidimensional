324599cb1bd8e1386d2eb1efa5e07152def2e80d
Filipe Viana Monteiro <filipeviana_@live.com>
2019-06-28 16:15:51 -0300

    separando em packages

--- __init__.py
+++ __init__.py
@@ -1,66 +1,27 @@
-import requests 
-from bs4 import BeautifulSoup
+from scrapping import get_list_of_ads
+from scrapping import get_ad_info
 
 url = 'https://rn.olx.com.br/rio-grande-do-norte/natal/celulares'
 
-def get_list_of_ads(url=''):
-	#realiza request na pagina da OLX
-	r = requests.get(url)
+if __name__ == '__main__':
 
-	#checa se a pagina foi recebida com sucesso
-	if r.status_code == 200:
-		html_doc = r.text
-	else:
-		return []
+	pare_o_loop = True #condicao para finalizar o while que resgata a lista de links de produtos
+	list_all = [] #lista de urls de produtos
+	i = 1 #contador de página
 
-	#transforma pagina recebida em um obj do bs4 
-	soup = BeautifulSoup(html_doc, 'html.parser')
+	while pare_o_loop:
+		print('tentando resgatar a lista ' + str(i))
+		listing = get_list_of_ads(url + '?o=' + str(i))
+		if not listing:
+			pare_o_loop = False
+		else:
+			i += 1
+			list_all += listing
 
-	#resgata os elementos que contem os anuncios da página do OLX
-	anuncios = soup.findAll('a', {"class": 'OLXad-list-link'})
+	info_ads = dict()
 
-	#inicializa a lista de links
-	link_list = []
+	for i in list_all:
+		print(get_ad_info(i)) #imprime as informações resgatadas no anuncio
 
-	for anuncio in anuncios:
-		link_list.append(anuncio['href'])
-
-	return link_list
-
-def get_ad_info(url=''):
-	r = requests.get(url)
-
-	if r.status_code == 200:
-		#faca aqui
-		soup = BeautifulSoup(r.text, 'html.parser')
-		info = dict()
-		info['tilulo'] = soup.findAll('h1', {'class': 'OLXad-title'})[0].string
-		info['preco'] = soup.findAll('span', {'class': 'actual-price'})[0].string
-		return info
-	else:
-		return {}
-
-pare_o_loop = True
-
-list_all = []
-i = 1
-
-while pare_o_loop:
-	print('tentando resgatar a lista ' + str(i))
-	listing = get_list_of_ads(url + '?o=' + str(i))
-	if not listing or i == 2:
-		pare_o_loop = False
-	else:
-		i += 1
-		list_all += listing
-
-info_ads = dict()
-
-for i in list_all:
-	print(get_ad_info(i))
-
-print(list_all)
-
-#imprime a pagina recebida
-#print(soup.prettify())
+	#TODO: persistir a base de dados de anuncios resgatos em um arquivo .csv
 
---
+++ scrapping.py
@@ -0,0 +1,44 @@
+import requests 
+from bs4 import BeautifulSoup
+
+#dada a URL da página de anuncios da OLX retorna a lista de links do anuncios da página
+def get_list_of_ads(url=''):
+	#realiza request na pagina
+	r = requests.get(url)
+
+	#checa se a pagina foi recebida com sucesso
+	if r.status_code == 200:
+		html_doc = r.text #instancia a variavel html_doc com o HTML recebido
+	else:
+		return [] #em caso de falha no recebimento da página retorna lista vazia
+
+	#transforma pagina recebida em um obj do bs4 
+	soup = BeautifulSoup(html_doc, 'html.parser') #faz o parse do HTML da página recebida
+
+	#resgata os elementos que contem os anuncios da página do OLX
+	anuncios = soup.findAll('a', {"class": 'OLXad-list-link'})
+
+	#inicializa a lista de links
+	ad_link = []
+
+	for anuncio in anuncios:
+		ad_link.append(anuncio['href']) # inclui o link do objeto HTML do anuncio na lista 
+
+	#retorna a lista de links de anuncio
+	return ad_link
+
+#retorna metadados do anuncio dado o link da página do anuncio da OLX
+def get_ad_info(url=''):
+	r = requests.get(url) #realiza request na página
+
+	#checa se a página foi carregada com sucesso
+	if r.status_code == 200:
+		soup = BeautifulSoup(r.text, 'html.parser') #faz o parse da página HTML
+		info = dict() #instancia o dicionário que será retornado
+		info['tilulo'] = soup.findAll('h1', {'class': 'OLXad-title'})[0].string #inclui o titulo do anuncio no DICT
+		info['preco'] = soup.findAll('span', {'class': 'actual-price'})[0].string #inclui o preço do anuncio no DICT
+		#TODO: inclui demais metadados do anuncio no DICT
+		return info
+	#em caso de falha no carregamento da página retorna DICT vazio
+	else:
+		return {}
