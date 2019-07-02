9433364e48544306e0d045ad72e05964052ad892
Thiago de Oliveira <oliveira.thiago.residente@tre-rn.jus.br>
2019-07-02 17:50:03 -0300

    Limpeza dos dados usando pandas

--- web-scrapping/__init__.py
+++ web-scrapping/__init__.py
@@ -1,5 +1,6 @@
 from scrapping import get_list_of_ads
 from scrapping import get_ad_info
+import pandas as pd
 
 url = 'https://rn.olx.com.br/rio-grande-do-norte/natal/celulares'
 
@@ -7,7 +8,7 @@ if __name__ == '__main__':
 
 	pare_o_loop = True #condicao para finalizar o while que resgata a lista de links de produtos
 	list_all = [] #lista de urls de produtos
-	i = 1 #contador de página
+	i = 99 #contador de página
 
 	while pare_o_loop:
 		print('tentando resgatar a lista ' + str(i))
@@ -24,5 +25,14 @@ if __name__ == '__main__':
 		info_ads.append(get_ad_info(i)) #imprime as informações resgatadas no anuncio
 
 	#TODO: persistir a base de dados de anuncios resgatos em um arquivo .csv
-	df = pd.DataFrame(lista)
+	df = pd.DataFrame(info_ads).dropna()
+
+	df['preco'] = df['preco'].str.replace('[R$ .]', '')
+
+	df['tilulo'] = df['tilulo'].str.replace('([\n\t])', '')
+	df['tilulo'] = df['tilulo'].str.replace('(\s{3,})', '')
+
+	df['preco'] = df['preco'].astype(str).astype(int)
+    
+    
 	df.to_csv('resultados.csv')
